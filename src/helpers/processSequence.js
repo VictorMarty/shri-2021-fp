c25a03e5ba2c633a39a67762e0f3a271294576da
Victor Martynov <vicmart143@gmail.com>
2021-11-13 03:36:08 +0700

    finish second part homework

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,102 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
+import { round, toNumber, toString } from 'lodash';
+import { __, assoc, allPass, compose, gt, ifElse, lt, prop, tap, andThen, otherwise, modulo, concat } from 'ramda';
 import Api from '../tools/api';
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+    // props
+    const getLength = prop('length')
+    const getResult = prop('result')
+    // logs
+    const logErrorValidation = ()  => handleError("ValidationError")
+    const logErrorPromise = (e) => handleError(e)
+    const logSuccessPromise = (message) => handleSuccess(message)
+    // Validators
+    const lengthLess10 = compose(gt(10), getLength)
+    const isPositiveNumber = compose(lt(0), Number)
+    const lengthMore2 = compose(lt(2), getLength)
+    const isValid = allPass([
+        lengthLess10,
+        isPositiveNumber,
+        lengthMore2
+    ])
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+    // operations
+    const toSquare = (n) => n**2
+    const mod3 = modulo(__, 3);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+    const apiAnimals = api.get(__,{})
+    const createUrlAnimals = concat("https://animals.tech/")
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+    const logAnimal = compose(logSuccessPromise,getResult)
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+    const getAnimals = compose(
+        otherwise(logErrorPromise),
+        andThen(logAnimal),
+        apiAnimals,
+        createUrlAnimals,
+        toString
+    )
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+    const useBinaryValue = compose(
+        getAnimals,
+        tap(writeLog),
+        mod3,
+        tap(writeLog),
+        toSquare,
+        tap(writeLog),
+        getLength,
+        tap(writeLog),
+        getResult
+    )
+
+    const apiNumber = api.get("https://api.tech/numbers/base")
+    const setParams = assoc("number", __, { from: "10", to: "2" })
+    const getBinaryValue = compose(
+        apiNumber,
+        setParams,
+        toString
+    )
+
+    const getIntegerValue = compose(
+        tap(writeLog),
+        round,
+        toNumber
+    )
+
+    const useValue = compose(
+        otherwise(logErrorPromise),
+        andThen(useBinaryValue),
+        getBinaryValue,
+        getIntegerValue,
+    )
+
+    const validate = ifElse(
+        isValid,
+        useValue,
+        logErrorValidation
+    )
+
+    const process = compose(validate, tap(writeLog))
+
+    process(value)
+};
 
 export default processSequence;
